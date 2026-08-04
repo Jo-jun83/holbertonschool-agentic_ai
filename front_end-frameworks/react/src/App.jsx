fff55f6d8dd4b9f061ff6821aef572b56a316bb7
Jonas-Jungling <jonas.jungling@oclock.school>
2026-08-04 10:03:28 +0200

    set up project

---
+++ front_end-frameworks/react/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { Check } from 'lucide-react';
+
+
+function App() {
+  return (
+    <>
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-4xl font-bold">Introduction to React</h1>
+          <h2 className="text-2xl">Task 0</h2>
+          <Check />
+        </div>
+        
+    </>
+  )
+}
+
+export default App
