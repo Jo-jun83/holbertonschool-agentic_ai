fa4e4696b1061fb41c7c5bfdebfa815357f14db2
Jonas-Jungling <jonas.jungling@oclock.school>
2026-08-11 15:33:09 +0200

    Initialize Svelte project with Vite, Tailwind CSS, and Lucide icons

---
+++ front_end-frameworks/svelte/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Eye from '@lucide/svelte/icons/eye';
+</script>
+
+<section id="center" class="flex flex-col items-center">
+  <h1>Svelte</h1>
+  <h2>Introduction to Svelte</h2>
+  <Eye />
+</section>
